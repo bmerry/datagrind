6f0a9cdec52d5f9d6136fc30283943ac40775cfc
Bruce Merry <bmerry@users.sourceforge.net>
2010-09-12 22:56:39 +0100

    Bugfixes to debuginfo

--- exp-datagrind/dg_view_debuginfo.cpp
+++ exp-datagrind/dg_view_debuginfo.cpp
@@ -30,35 +30,83 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <memory>
 #include <bfd.h>
 
 using namespace std;
 
-struct object_file
+namespace
 {
+
+/* Corresponds to either the main file or the .gnu_debuglink file */
+class object_subfile
+{
+public:
+    char *filename;  /* bfd uses this storage rather than making its own */
+    bfd *abfd;
+    vector<asymbol *> syms;
+
+private:
+    /* Prevent copying */
+    object_subfile(const object_subfile &);
+    object_subfile &operator=(const object_subfile &);
+
+public:
+    object_subfile() : filename(NULL), abfd(NULL), syms() {}
+    ~object_subfile()
+    {
+        delete[] filename;
+        if (abfd != NULL)
+            bfd_close(abfd);
+    }
+
+    bool load(const char *filename);
+};
+
+class object_file
+{
+public:
     HWord text_avma;
-    bfd *abfd[2];                /* Main file and .gnu_debuglink */
-    vector<asymbol *> syms[2];
+    object_subfile *subfiles[2];
 
     object_file() : text_avma(0)
     {
-        abfd[0] = abfd[1] = NULL;
+        subfiles[0] = subfiles[1] = NULL;
     }
+
+    ~object_file()
+    {
+        delete subfiles[0];
+        delete subfiles[1];
+    }
+
+private:
+    /* Prevent copying */
+    object_file(const object_file &);
+    object_file &operator=(const object_file &);
 };
 
-static map<string, object_file> object_files;
+/* TODO: this never gets cleaned up */
+static map<string, object_file *> object_files;
 
 /* Loads either the main file or the .gnu_debuglink file */
-static bool load_single_object_file(const char *filename, bfd *&abfd, vector<asymbol *> &syms)
+bool object_subfile::load(const char *filename)
 {
     char **matching;
     long symcount = 0, storage;
+    size_t filename_len = strlen(filename);
 
-    abfd = bfd_openr(filename, NULL);
+    delete[] this->filename;
+    if (abfd != NULL)
+        bfd_close(abfd);
+
+    this->filename = new char[filename_len + 1];
+    strcpy(this->filename, filename);
+    abfd = bfd_openr(this->filename, NULL);
     if (!abfd)
         return false;
     if (!bfd_check_format_matches(abfd, bfd_object, &matching))
-        goto bad_close;
+        return false;
 
     storage = bfd_get_symtab_upper_bound(abfd);
     if (storage > 0)
@@ -77,42 +125,40 @@ static bool load_single_object_file(const char *filename, bfd *&abfd, vector<asy
         }
     }
     if (symcount <= 0)
-        goto bad_close;
+        return false;
 
     return true;
 
-bad_close:
     bfd_close(abfd);
     abfd = NULL;
     return false;
 }
 
+} /* namespace */
+
 void load_object_file(const char *filename, HWord text_avma)
 {
-    vector<asymbol *> syms;
-    bfd *abfd = NULL;
+    auto_ptr<object_subfile> primary(new object_subfile);
+    if (!primary->load(filename))
+        return;
 
-    if (load_single_object_file(filename, abfd, syms))
+    object_file *&of = object_files[filename];
+    if (of == NULL)
+        of = new object_file;
+    of->text_avma = text_avma;
+    delete of->subfiles[0];
+    delete of->subfiles[1];
+    of->subfiles[0] = primary.release();
+    of->subfiles[1] = NULL;
+    primary.release();
+
+    char *gnu_debuglink = bfd_follow_gnu_debuglink(of->subfiles[0]->abfd, "/usr/lib/debug");
+    if (gnu_debuglink != NULL)
     {
-        object_file &of = object_files[filename];
-        if (of.abfd[0] != NULL)
-            bfd_close(of.abfd[0]);
-        of.text_avma = text_avma;
-        of.abfd[0] = abfd;
-        of.syms[0].swap(syms);
-
-        char *gnu_debuglink = bfd_follow_gnu_debuglink(abfd, "/usr/lib/debug");
-        if (gnu_debuglink != NULL)
-        {
-            if (load_single_object_file(gnu_debuglink, abfd, syms))
-            {
-                if (of.abfd[1] != NULL)
-                    bfd_close(of.abfd[1]);
-                of.syms[1].swap(syms);
-                of.abfd[1] = abfd;
-            }
-            free(gnu_debuglink);
-        }
+        auto_ptr<object_subfile> secondary(new object_subfile);
+        if (secondary->load(gnu_debuglink))
+            of->subfiles[1] = secondary.release();
+        free(gnu_debuglink);
     }
 }
 
@@ -120,10 +166,10 @@ struct addr2line_info
 {
     HWord addr;
     object_file *obj;
-    int pass;
+    object_subfile *sub;
     bool found;
-    const char *file_name;
-    const char *function_name;
+    const char *source;
+    const char *function;
     unsigned int line;
 };
 
@@ -140,43 +186,54 @@ static void addr2line_section(bfd *abfd, asection *sect, void *arg)
     if (info->addr >= info->obj->text_avma + size)
         return;
 
-    info->found = bfd_find_nearest_line(abfd, sect, &info->obj->syms[info->pass][0],
-                                        info->addr - info->obj->text_avma,
-                                        &info->file_name, &info->function_name,
-                                        &info->line);
+    if (!bfd_find_nearest_line(abfd, sect, &info->sub->syms[0],
+                               info->addr - info->obj->text_avma,
+                               &info->source, &info->function,
+                               &info->line))
+    {
+        info->source = NULL;
+        info->function = NULL;
+        info->line = 0;
+    }
+    info->found = true;
 }
 
 string addr2line(HWord addr)
 {
     ostringstream label;
     label << hex << showbase << addr << dec << noshowbase;
-    for (map<string, object_file>::iterator i = object_files.begin(); i != object_files.end(); ++i)
+    for (map<string, object_file *>::iterator i = object_files.begin(); i != object_files.end(); ++i)
     {
-        object_file &of = i->second;
-        if (addr >= of.text_avma)
+        object_file *of = i->second;
+        if (addr >= of->text_avma)
         {
             addr2line_info info;
 
             info.addr = addr;
-            info.obj = &of;
+            info.obj = of;
             info.found = false;
-            for (info.pass = 1; info.pass >= 0; info.pass--)
+            for (int pass = 1; pass >= 0; pass--)
             {
-                if (of.abfd[info.pass] == NULL) continue;
-                bfd_map_over_sections(of.abfd[info.pass], addr2line_section, &info);
+                object_subfile *osf = of->subfiles[pass];
+                if (osf == NULL) continue;
+
+                info.sub = osf;
+                bfd_map_over_sections(osf->abfd, addr2line_section, &info);
                 if (info.found)
                 {
-                    if (info.function_name != NULL && info.function_name[0])
-                        label << " in " << info.function_name;
+                    if (info.function != NULL && info.function[0])
+                        label << " in " << info.function;
                     label << " (";
-                    if (info.file_name)
+                    if (info.source)
                     {
-                        const char *suffix = strrchr(info.file_name, '/');
+                        const char *suffix = strrchr(info.source, '/');
                         if (suffix == NULL)
-                            suffix = info.file_name;
+                            suffix = info.source;
                         else
                             suffix++; /* Skip over the last / */
-                        label << suffix << ":" << info.line;
+                        label << suffix;
+                        if (info.line != 0)
+                            label << ":" << info.line;
                     }
                     else
                         label << i->first;
